95a4db85fd6cef7fc24ec8f031899d9b6202b5f4
Barry Ross <barry@4d-media.ca>
2017-10-27 15:29:40 -0700

    active menu items

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,36 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Scroll from 'react-scroll'; // Imports all Mixins
 
-const handleSetActive = (evt)=>{
-  //TODO set active link to display
-}
-const Header =  () => {
-  let Link = Scroll.Link;
 
-  return (
-    <header class="flex dark">
-          <div class="inner-wrapper">
-            <div class="logo-container">
-              <div class="logo">
-                <span>4d Media</span>
-              </div>
-              <div class="hamburger">
-                Menu
+export default class Header extends Component  {
+  constructor(props){
+  	super(props);
+  	this.state = {
+      activeItem: "about"
+    };
+  }
+  render(){
+    let Link = Scroll.Link;
+
+    const menu = ["about", "services", "portfolio", "contact"];
+    return (
+      <header class="flex dark">
+            <div class="inner-wrapper">
+              <div class="logo-container">
+                <div class="logo">
+                  <span>4d Media</span>
+                </div>
+                <div class="hamburger">
+                  Menu
+                </div>
               </div>
             </div>
-          </div>
-          <nav>
-            <ul class="flex">
-              <li><Link to="about" activeClass="active" spy={true} smooth={true} offset={50} duration={500}  >About</Link></li>
-              <li><Link to="services" activeClass="active" spy={true} smooth={true} offset={50} duration={500} >Services</Link></li>
-              <li><Link to="portfolio" activeClass="active" spy={true} smooth={true} offset={50} duration={500}  >Portfolio</Link></li>
-              {/* <li><Link to="testimonials" activeClass="active" spy={true} smooth={true} offset={50} duration={500} >Testimonials</Link></li>
-              <li><Link to="pricing" activeClass="active" spy={true} smooth={true} offset={50} duration={500} >Pricing</Link></li> */}
-              <li><Link to="contact" activeClass="active" spy={true} smooth={true} offset={50} duration={500} >Contact</Link></li>
-            </ul>
-          </nav>
-        </header>
-  )
-}
+            <nav>
+              <ul class="flex">
+                {
+                  menu.map((item)=><li><Link onClick={()=>this.handleSetActive(item)} to={item} className={this.state.activeItem == item ? "active": ""} spy={true} smooth={true} offset={50} duration={500} >{item}</Link></li>
+                )
+                }
+              </ul>
+            </nav>
+          </header>
+    )
+  }
+  handleSetActive = (item)=>{
+    this.setState({activeItem:item})
+  }
 
-export default Header
+}
