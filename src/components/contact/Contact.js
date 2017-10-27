ffe6f6f020fa487d718ea26fd2d4fe62f971042d
Barry Ross <barry@4d-media.ca>
2017-10-26 17:40:01 -0700

    gets form working!

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,6 @@
 
 import React, {Component} from 'react';
-import { Form } from 'semantic-ui-react';
+import { Form, Message } from 'semantic-ui-react';
 import SubscribeFrom from 'react-mailchimp-subscribe';
 
 const options = [
@@ -11,27 +11,69 @@ const options = [
 
 
 export default class Contact extends Component{
-  state = {}
+
+  constructor(props){
+    super(props)
+    this.state = {
+        fname:"",
+        lname:"",
+        email:"",
+        message:"",
+        formSuccess:false
+      }
+  }
+
+  onChange = (e) => {
+    const state = this.state
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  }
 
   handleChange = (e, { value }) => this.setState({ value })
-
+  handleSubmit = () => {
+    fetch('https://formspree.io/barry@4d-media.ca', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state)
+    })
+    .then(()=>{
+      this.setState(
+        {
+          fname:"",
+          lname:"",
+          email:"",
+          message:"",
+          formSuccess:true
+        }
+      )
+    })
+  }
   render(){
     const { value } = this.state
+    const {fname, lname, email, message}  = this.state;
 
     return (
       <section id="contact">
         <FormComp/>
         <h1>Contact</h1>
-      <Form action="https://4d-media.us6.list-manage.com/subscribe/post">
-          <Form.Group widths='equal'>
-            <Form.Input label='First name' placeholder='First name' />
-            <Form.Input label='Last name' placeholder='Last name' />
-            <Form.Input label='Email' placeholder='Email' />
-          </Form.Group>
-          <Form.TextArea label='About' placeholder='Tell us more about you...' />
-          <Form.Checkbox label='I agree to the Terms and Conditions' />
+      <Form success={this.state.formSuccess} onSubmit={this.handleSubmit}>
+        <Message
+             success
+             header='Form Completed'
+             content="Thank you for your submission.  I will be in touch shortly!"
+           />
+            <Form.Group widths='equal'>
+              <Form.Input name="fname" value={fname} label='First name' placeholder='First name' onChange={this.onChange}/>
+            <Form.Input name="lname" value={lname} label='First name' placeholder='Last name' onChange={this.onChange}/>
+          <Form.Input name="email" value={email} label='Email' placeholder='Email' onChange={this.onChange}/>
+            </Form.Group>
+            <Form.TextArea name="message" value={message} label='Message' placeholder='What can I help you with?' onChange={this.onChange}/>
+
           <Form.Button>Submit</Form.Button>
-        </Form>
+          </Form>
        </section>
      )
   }
