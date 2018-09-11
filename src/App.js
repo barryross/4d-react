f47d64fb5c817e0ffe2d3472a37f6dfb8096309e
Barry Ross <barry@4d-media.ca>
2018-09-10 19:28:17 -0700

    removes testimonials

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
       <div className="App">
         <Header/>
         <Services/>
-        <Testimonials/>
+        {/* <Testimonials/> */}
         <About/>
         <Portfolio/>
         <Contact/>
