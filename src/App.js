4ace97530181d80b84a3e2626a2b70ceeb0d7fc5
Barry Ross <barry@4d-media.ca>
2019-01-23 09:39:33 -0800

    cleans up App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,30 @@
 import React, { Component } from 'react';
-import Header from './components/header/Header'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
-import Services from './components/services/Services'
-import Portfolio from './components/gallery/Gallery'
-import Timeline from './components/timeline/Timeline'
 import Footer from './components/footer/Footer'
-
-
+import Header from './components/header/Header'
+import Services from './components/services/Services'
+import Timeline from './components/timeline/Timeline'
 import Testimonials from './components/testimonials/Testimonials'
 
 import './styles/App.css';
 
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-
-
 class App extends Component {
-
-  constructor(props){
-    super(props)
-console.log("PROCESS", process.env)
-  }
-
-  componentDidMount(){
-
-  }
   render() {
-
     return (
-  
-
-      <MuiThemeProvider>
-      <div className="App">
-        <Header/>
-        <About/>
-        <Services/>
-        {/* <Portfolio/> */}
-        <Testimonials/>
-        <Contact/>
-        <Timeline/>
-        <Footer/>
-
-
-
-      </div>
-    </MuiThemeProvider>
+			<MuiThemeProvider>
+				<div className="App">
+					<Header/>
+					<About/>
+					<Services/>
+					<Testimonials/>
+					<Contact/>
+					<Timeline/>
+					<Footer/>
+				</div>
+			</MuiThemeProvider>
     );
   }
 }
