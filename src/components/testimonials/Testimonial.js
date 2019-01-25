7cb35185b26593e5959d68f8bb32ea8a4d519dfa
Barry Ross <barry@4d-media.ca>
2019-01-25 15:06:22 -0800

    Abstracts testominal display into Testimonial component

---
+++ src/components/testimonials/Testimonial.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import {DEFAULT_AVATAR} from '../../common/globals'
+
+export default({data}) => {
+    return (
+        <blockquote>
+            <p>
+                <i class="fa fa-quote-left" aria-hidden="true"></i>
+                {data.content}
+            </p>
+            <footer>
+                <div>
+                    <img
+                        alt="avatar"
+                        class="avatar"
+                        src={data.avatar ? data.avatar.secure_url : DEFAULT_AVATAR}
+										/>
+                    <cite>{data.name.first} {data.name.last}</cite>
+                </div>
+                <a target="_blank" href={data.website}>{data.website}</a>
+            </footer>
+        </blockquote>
+    )
+}
