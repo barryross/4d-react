7028e5a6644150c3aa45372559d5a6c4dd7035c7
Barry Ross <barry@4d-media.ca>
2019-01-25 15:38:15 -0800

    Cleans up Contact component

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,74 +1,80 @@
-
 import React, {Component} from 'react';
-import { Form, Message } from 'semantic-ui-react';
+import {Form, Message} from 'semantic-ui-react';
+import {FORMSPREE_URI} from '../../common/globals'
 
+export default class Contact extends Component {
 
-export default class Contact extends Component{
-
-  constructor(props){
-    super(props)
-    this.state = {
-        fname:"",
-        lname:"",
-        email:"",
-        message:"",
-        formSuccess:false
-      }
-  }
-
-  onChange = (e) => {
-    const state = this.state
-    state[e.target.name] = e.target.value;
-    this.setState(state);
-  }
-
-  handleChange = (e, { value }) => this.setState({ value })
-  handleSubmit = () => {
-    fetch('https://formspree.io/barry@4d-media.ca', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(this.state)
-    })
-    .then(()=>{
-      this.setState(
-        {
-          fname:"",
-          lname:"",
-          email:"",
-          message:"",
-          formSuccess:true
+    constructor(props) {
+        super(props)
+        this.state = {
+            fname: "",
+            lname: "",
+            email: "",
+            message: "",
+            formSuccess: false
         }
-      )
-    })
-  }
-  render(){
-    const { value } = this.state
-    const {fname, lname, email, message}  = this.state;
+    }
 
-    return (
-      <section id="contact">
-          <h1>Contact</h1>
-          <div className="form-container">    
-              <Form success={this.state.formSuccess} onSubmit={this.handleSubmit}>
-              <Message
-                 success
-                 content="Thank you for your submission.  I will be in touch shortly!"
-               />
-                <Form.Group widths='equal'>
-                 <Form.Input name="fname" value={fname} label='First name' placeholder='First name' onChange={this.onChange}/>
-                 <Form.Input name="lname" value={lname} label='First name' placeholder='Last name' onChange={this.onChange}/>
-                </Form.Group>
-                <Form.Input name="email" value={email} label='Email' placeholder='Email' onChange={this.onChange}/>
+    onChange = (e) => {
+        const state = this.state
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    }
 
-                <Form.TextArea name="message" value={message} label='Message' placeholder='What can I help you with?' onChange={this.onChange}/>
-               <Form.Button>Submit</Form.Button>
-              </Form>
-          </div>
-       </section>
-     )
-  }
+    handleChange = (e, {value}) => this.setState({value})
+    handleSubmit = () => {
+        fetch(`${FORMSPREE_URI}`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state)
+        }).then(() => {
+            this.setState({fname: "", lname: "", email: "", message: "", formSuccess: true})
+        })
+    }
+    render() {
+        const {fname, lname, email, message} = this.state;
 
+        return (
+            <section id="contact">
+                <h1>Contact</h1>
+                <div className="form-container">
+                    <Form success={this.state.formSuccess} onSubmit={this.handleSubmit}>
+                        <Message
+                            success
+                            content="Thank you for your submission.  I will be in touch shortly!"/>
+                        <Form.Group widths='equal'>
+                            <Form.Input
+                                name="fname"
+                                value={fname}
+                                label='First name'
+                                placeholder='First name'
+                                onChange={this.onChange}/>
+                            <Form.Input
+                                name="lname"
+                                value={lname}
+                                label='First name'
+                                placeholder='Last name'
+                                onChange={this.onChange}/>
+                        </Form.Group>
+                        <Form.Input
+                            name="email"
+                            value={email}
+                            label='Email'
+                            placeholder='Email'
+                            onChange={this.onChange}/>
+                        <Form.TextArea
+                            name="message"
+                            value={message}
+                            label='Message'
+                            placeholder='What can I help you with?'
+                            onChange={this.onChange}/>
+                        <Form.Button>Submit</Form.Button>
+                    </Form>
+                </div>
+            </section>
+        )
+    }
 }
