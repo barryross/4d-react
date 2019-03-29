b436b64148a9bd160069c38df4cde8a82dad261d
Barry Ross <barry@4d-media.ca>
2019-03-28 18:50:16 -0700

    Cleans up Timeline

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,43 +1,42 @@
 import React, {Component} from 'react';
-import {API_URI} from '../../common/globals'
 import axios from 'axios'
-import TimelineElement from './TimelineElement/'
-import {VerticalTimeline} from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import {VerticalTimeline} from 'react-vertical-timeline-component';
+
+import {API_URI} from '../../common/globals'
+import TimelineElement from './TimelineElement/'
 
 export class Timeline extends Component {
-    constructor(props) {
-        super(props)
+	constructor(props) {
+			super(props)
 
-        this.state = {
-            timelineElements: []
-        }
-    }
+			this.state = {
+					timelineElements: []
+			}
+	}
 
-    async componentDidMount() {
-        let res = await axios.get(`${API_URI}/timeline`)
-        this.setState({timelineElements: res.data.timeline})
-    }
-
-    render() {
-        let {timelineElements} = this.state
-        return (
-            <section id="timeline" class="primary-section">
-											<div class="section-header-wrapper">
-
-                <h1>Timeline</h1>
-                    <h2>What's my story?</h2>
-                </div>
-								<div className="inner-content-wrapper">
-                <VerticalTimeline>
-                    {timelineElements.map(item => {
-                        return (<TimelineElement item={item}/>)
-                    })}
-                </VerticalTimeline>
-								</div>
-            </section>
-        )
-    }
+	async componentDidMount() {
+			let res = await axios.get(`${API_URI}/timeline`)
+			this.setState({timelineElements: res.data.timeline})
+	}
+  render() {
+			let {timelineElements} = this.state
+			return (
+			<section id="timeline" class="primary-section">
+				<div class="section-header-wrapper">
+					<h1>Timeline</h1>
+					<h2>What's my story?</h2>
+				</div>
+				<div className="inner-content-wrapper">
+					<VerticalTimeline>
+					{timelineElements.map(item => {
+						return (<TimelineElement item={item}/>)
+					})}
+					</VerticalTimeline>
+				</div>
+			</section>
+			)
+  }
 }
 
 export default Timeline
