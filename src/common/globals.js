3ab448067cf36ebeae5ba7dd9cc081948ccb09b6
Barry Ross <barry@4d-media.ca>
2019-03-28 18:50:50 -0700

    Updates globals

--- src/common/globals.js
+++ src/common/globals.js
@@ -1,1 +1,14 @@
 export const API_URI = "https://infinite-beach-14551.herokuapp.com/api/v1";
+export const FORMSPREE_URI = "https://formspree.io/barry@4d-media.ca";
+export const DEFAULT_AVATAR = "https://i2.wp.com/www.ahfirstaid.org/wp-content/uploads/2014/07/avatar-placeholder.png?w=204";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/barry-ross-a3b19520/"
+export const GITHUB_URL = "https://www.github.com/barryross"
+export const SLIDER_SETTINGS = {
+	dots: true,
+	infinite: true,
+	speed: 3000,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	adaptiveHeight: true
+};
